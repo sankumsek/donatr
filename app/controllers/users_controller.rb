c5991705904aa54bd5c6c194f38ab03e3c27f8fb
Nick Bernard <nick@nrbernard.com>
2014-09-02 16:15:05 -0700

    edit user profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,19 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      redirect_to @user, notice: "Profile updated"
+    else
+      render "edit"
+    end
+  end
+
 private
   def user_params
     params.require(:user).permit(:name, :password, :password_confirmation)
