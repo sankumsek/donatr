0b02d1698cb022e2605ee561599684e3e5a6ffa4
Nick Bernard <nick@nrbernard.com>
2014-09-02 11:43:27 -0700

    edit and delete causes

--- app/controllers/causes_controller.rb
+++ app/controllers/causes_controller.rb
@@ -18,6 +18,29 @@ class CausesController < ApplicationController
     end
   end
 
+  def show
+    @cause = Cause.find(params[:id])
+  end
+
+  def edit
+    @cause = Cause.find(params[:id])
+  end
+
+  def update
+    @cause = Cause.find(params[:id])
+    if @cause.update_attributes(cause_params)
+      redirect_to @cause, notice: "Cause updated"
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @cause = Cause.find(params[:id])
+    @cause.destroy
+    redirect_to root_url, notice: "Cause deleted"
+  end
+
 private
   def cause_params
     params.require(:cause).permit(:name, :description)
